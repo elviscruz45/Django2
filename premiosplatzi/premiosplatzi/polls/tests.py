ead5156e9679ef4adaef71287f80654146a445f5
Elvis <elviscruz45@gmail.com>
2022-08-19 14:26:05 -0500

    primer test y correccion

--- premiosplatzi/premiosplatzi/polls/tests.py
+++ premiosplatzi/premiosplatzi/polls/tests.py
@@ -1,2 +1,14 @@
-from django.test import TestCase
-# Create your tests here.
+import datetime
+from django.test import TestCase  # Create your tests here.
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModeltests(TestCase):
+    def test_was_publish_recently_with_future_questions(self):
+        """was_published_recently return False for questions whose pub_date is in the future"""
+        time=timezone.now()+datetime.timedelta(days=30)
+        future_question=Question(question_text="Quien es el mejor course Director de Platzi",pub_date=time)
+        self.assertIs(future_question.was_published_recently(),False)
+        
+
