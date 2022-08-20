20b138c3baeb0b4985ec1cd18b77737ff4c0bca0
Elvis <elviscruz45@gmail.com>
2022-08-20 07:36:49 -0500

    final de curso intermedio

--- premiosplatzi/premiosplatzi/polls/views.py
+++ premiosplatzi/premiosplatzi/polls/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question,Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+
 
 
 # Create your views here.
@@ -31,18 +33,21 @@ class IndexView(generic.ListView):
     context_object_name="latest_question_list"
     def get_queryset(self) :
         """Return the last five published questions"""
-        return  Question.objects.order_by("-pub_date")[:5]
+#       return  Question.objects.order_by("-pub_date")[:5]
+        return  Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model=Question
     template_name="polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that arent published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
     
 class ResultView(generic.DetailView):
     model=Question
     template_name="polls/results.html"
-
-
-
 
 def votes(request, question_idis):
     question=get_object_or_404(Question,pk=question_idis)
